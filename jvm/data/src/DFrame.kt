d777547ade5366c996f25e24ffb2dfa3d3ae8ac0
Suresh G <sureshg.g@gmail.com>
2026-06-04 22:52:07 -0700

    chore: update Kotlin toolchain and fix dataframe and redacted compiler plugins

--- jvm/data/src/DFrame.kt
+++ jvm/data/src/DFrame.kt
@@ -1,14 +1,12 @@
 import org.jetbrains.kotlinx.dataframe.api.*
 
 fun main() {
-  println("Hello, Data!")
-
+  println("DataFrame Example!")
   val weatherData =
       dataFrameOf(
           "time" to columnOf(0, 1, 2, 4, 5, 7, 8, 9),
           "temperature" to columnOf(12.0, 14.2, 15.1, 15.9, 17.9, 15.6, 14.2, 24.3),
           "humidity" to columnOf(0.5, 0.32, 0.11, 0.89, 0.68, 0.57, 0.56, 0.5),
       )
-
-  weatherData.print()
+  weatherData.filter { temperature > 15.0 }.print()
 }
--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4015
+set kotlin_cli_version=0.12.0-dev-4019
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=fceeb2d16635e22e6c2084bf459e23de55059980f835a8a24d2d1e3426bc8292
+set kotlin_cli_sha256=22813fe1e012359b7061dca4833f89068b5583c57337a76151aeb406060110b3
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
