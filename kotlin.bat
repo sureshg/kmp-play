a7c51ab67fa6fef4209b3e8b92d1456d6e8ff549
Suresh G <sureshg.g@gmail.com>
2026-06-09 23:15:29 -0700

    chore: update Kotlin toolchain, enable intrinsic const evaluation, and upgrade dependencies

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4031
+set kotlin_cli_version=0.12.0-dev-4033
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=0f09e2ea15f2969bc06808d0cc5f62cc3611f50b47ab1109e7ac69072ec3e0eb
+set kotlin_cli_sha256=ed05f80c9d2b5a98799cff1a23a902c9cdfe32294768ac1e1fb3aa43192983fb
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
