4873fff44dd43d7e8adbec593023dce335edf81d
Suresh G <sureshg.g@gmail.com>
2026-07-09 18:19:47 -0700

    chore: update Kotlin toolchain and upgrade dependencies

---
+++ docs/script.main.kts
@@ -0,0 +1,59 @@
+#!/usr/bin/env -S kotlin -Xplugin=/opt/homebrew/opt/kotlin/libexec/lib/kotlinx-serialization-compiler-plugin.jar
+// @file:Repository("https://maven.google.com")
+@file:DependsOn("io.ktor:ktor-client-core:3.5.1")
+@file:DependsOn("io.ktor:ktor-client-cio:3.5.1")
+@file:DependsOn("io.ktor:ktor-client-java:3.5.1")
+@file:DependsOn("io.ktor:ktor-client-auth:3.5.1")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-datetime:0.8.0")
+@file:DependsOn("org.slf4j:slf4j-simple:2.0.18")
+@file:DependsOn("com.microsoft.playwright:playwright:1.61.0")
+// @file:Import("common.main.kts")
+
+@file:CompilerOptions("-jvm-target", "21")
+
+import com.microsoft.playwright.*
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable data class Lang(val name: String, val version: String)
+
+val arg = args.firstOrNull() ?: "Kotlin"
+
+println("Hello $arg!")
+
+val serialized = Json.encodeToString(Lang("Kotlin", KotlinVersion.CURRENT.toString()))
+
+println(serialized)
+
+val javaVer: String = System.getProperty("java.version")
+val deserialized = Json.decodeFromString<Lang>("""{"name" : "Java", "version": "$javaVer"}""")
+
+println(deserialized)
+
+runBlocking {
+  delay(1.seconds)
+  println("Done!")
+}
+
+fun recordBrowser() {
+  CLI.main(arrayOf("codegen", "-o", "record.java"))
+}
+
+fun playbackBrowser() {
+  Playwright.create().use { playwright ->
+    val browser = playwright.chromium().launch(BrowserType.LaunchOptions().setHeadless(true))
+
+    browser.newContext().newPage().apply {
+      navigate("https://www.google.com/search?q=kotlinlang&oq=kotlinlang")
+      click("text=Kotlin Programming Language")
+      assert(url() == "https://kotlinlang.org/")
+      click("#kotlin-code-example-simplest div div div div")
+      click("text=[X]")
+      close()
+    }
+  }
+}
--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4116
+set kotlin_cli_version=0.12.0-dev-4129
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=5b71b77cdf259948ccc9d5cebe9719f219f1f81d3c305dc2f593e0a98fbce13e
+set kotlin_cli_sha256=d7a6aea8970257dde4945e9014fa55fe0d997f7f1e458e8ec17c2eef7cfda95c
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
