da3e1023f78bd18e043cb07648416483ae1ad2cd
Suresh G <sureshg.g@gmail.com>
2026-07-28 19:46:44 -0700

    chore: update Kotlin toolchain and upgrade dependencies (Compose 1.12.0-beta03, kotlin-wrappers 2026.7.7)

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4179
+set kotlin_cli_version=0.12.0-dev-4184
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=bc94db04daccb39f7c0a659bd167e77e17ba5ecad6f3dfd51f343e6ad9e49d21
+set kotlin_cli_sha256=6f3f711117859ab64095e9dee1b21ce71c7e628709883822b0b1ff122d6e9113
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
