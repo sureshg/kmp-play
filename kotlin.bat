b72177c2c540b0cfb1fe8703a80893a3b1524890
Suresh G <sureshg.g@gmail.com>
2026-08-05 22:28:50 -0700

    chore: cinterop changes

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4220
+set kotlin_cli_version=0.12.0-dev-4222
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=c6c6022714abe6cddc4660c43e4dcb2217c48e27c5cb297541d1eaacd5087256
+set kotlin_cli_sha256=c1c967a76ae0927358d6244b1d12028ac5bd2afbdaa0950b92cd44bd0ad33a38
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
--- native/cinterop/src/Main.kt
+++ native/cinterop/src/Main.kt
@@ -1,13 +1,18 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
 import dev.suresh.custom.*
 import kotlinx.cinterop.*
 import libcurl.*
 
-@OptIn(ExperimentalForeignApi::class)
 fun main(args: Array<String>) {
-  println(getGreeting()?.toKString())
+  println(customGreeting)
+  fetchAndPrintUrl("https://suresh.dev/media-api/images.json")
+}
+
+fun fetchAndPrintUrl(url: String) {
   val curl = curl_easy_init()
   if (curl != null) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://suresh.dev/media-api/images.json")
+    curl_easy_setopt(curl, CURLOPT_URL, url)
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
     val res = curl_easy_perform(curl)
     if (res != CURLE_OK) {
@@ -16,3 +21,6 @@ fun main(args: Array<String>) {
     curl_easy_cleanup(curl)
   }
 }
+
+val customGreeting
+  get() = getGreeting()?.toKString()
