ff75d6a1503dbde6d9ceb36086bd832cd954ad1e
Suresh G <sureshg.g@gmail.com>
2026-08-12 12:18:40 -0700

    chore: update Kotlin 2.4.20-RC, toolchain, dependencies

--- shared/src/dev/suresh/rpc/MyService.kt
+++ shared/src/dev/suresh/rpc/MyService.kt
@@ -1,9 +1,10 @@
 package dev.suresh.rpc
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.rpc.annotations.Rpc
 
-@Rpc
+// import kotlinx.rpc.annotations.Rpc
+
+// @Rpc
 interface MyService {
   fun data(input: String): Flow<String>
 
