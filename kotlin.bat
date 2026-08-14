5c33ffe886cdd61908d8334d7ebf429679958191
Suresh G <sureshg.g@gmail.com>
2026-08-13 19:00:20 -0700

    chore: simplify macos target, update Kotlin toolchain & dependencies

--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4245
+set kotlin_cli_version=0.12.0-dev-4253
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=2f32ed149f7ed954f411f72baa1f90b069ca4000a06ddc384ad8903448fbd6e9
+set kotlin_cli_sha256=e7121ee19b1079597651956898cc580bf42ee6f52d4c5ded3b74d60b0827e6ab
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
