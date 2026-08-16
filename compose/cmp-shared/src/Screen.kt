dbd795b01b223f3e9df4128b3c641387bb3d8af7
Suresh G <sureshg.g@gmail.com>
2026-08-16 00:47:01 -0700

    chore: introduce compose modules (jvm/wasm), update dependencies and Kotlin toolchain

--- compose/cmp-shared/src/Screen.kt
+++ compose/cmp-shared/src/Screen.kt
@@ -2,22 +2,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.singleWindowApplication
+import dev.suresh.gen.Res
 
 @Composable
 fun App() {
   var count by remember { mutableStateOf(0) }
+  var sharedText by remember { mutableStateOf("") }
+
+  LaunchedEffect(Unit) {
+    println("Reading the shared text...")
+    sharedText = Res.readBytes("files/shared-text.txt").decodeToString()
+  }
+
   MaterialTheme {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+      Text(text = sharedText)
       Button(onClick = { count++ }) { BasicText(text = "Click me!") }
       BasicText(
           text = "Click count: $count",
@@ -27,5 +36,3 @@ fun App() {
 
   SideEffect { println("Side Effect") }
 }
-
-fun main() = singleWindowApplication(title = "Compose Desktop App") { App() }
--- kotlin.bat
+++ kotlin.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kotlin Toolchain distribution to provision and use
-set kotlin_cli_version=0.12.0-dev-4254
+set kotlin_cli_version=0.12.0-dev-4258
 @rem Establish chain of trust from here by specifying the exact checksum of the Kotlin Toolchain distribution to be run
-set kotlin_cli_sha256=fee93114d22c41c2aaa07491c9c46663a7ce44a603de48804d2c5c4358e7f897
+set kotlin_cli_sha256=600ff093d271fb78de33b8e22f312bf82d3cc3c1995d2c6d68b721aaad512670
 
 if not defined KOTLIN_CLI_DOWNLOAD_ROOT set KOTLIN_CLI_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined KOTLIN_CLI_BOOTSTRAP_CACHE_DIR set KOTLIN_CLI_BOOTSTRAP_CACHE_DIR=%LOCALAPPDATA%\JetBrains\Kotlin\cli
