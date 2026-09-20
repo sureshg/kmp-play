13acc51390d2f0187123fb66f1df06a69642181e
Suresh G <sureshg.g@gmail.com>
2026-09-20 11:44:41 -0700

    chore: refine Vector operator functions, bump kotlin-diff to 1.4.0, and add ktor-version-catalog

--- shared/src@jvm/dev/suresh/ffm/Vector.kt
+++ shared/src@jvm/dev/suresh/ffm/Vector.kt
@@ -5,29 +5,27 @@ package dev.suresh.ffm
 import jdk.incubator.vector.*
 
 // Generic Vector-to-Vector Operations
+operator fun <E, V : Vector<E>> V.plus(other: V): V = add(other) as V
 
-operator fun <E, V : Vector<E>> V.plus(other: V): V = this.add(other) as V
+operator fun <E, V : Vector<E>> V.minus(other: V): V = sub(other) as V
 
-operator fun <E, V : Vector<E>> V.minus(other: V): V = this.sub(other) as V
+operator fun <E, V : Vector<E>> V.times(other: V): V = mul(other) as V
 
-operator fun <E, V : Vector<E>> V.times(other: V): V = this.mul(other) as V
+operator fun <E, V : Vector<E>> V.div(other: V): V = div(other) as V
 
-operator fun <E, V : Vector<E>> V.div(other: V): V = this.div(other) as V
-
-operator fun <E, V : Vector<E>> V.unaryMinus(): V = this.neg() as V
+operator fun <E, V : Vector<E>> V.unaryMinus(): V = neg() as V
 
 operator fun <E, V : Vector<E>> V.unaryPlus(): V = this
 
 // Index Access
+operator fun FloatVector.get(index: Int): Float = lane(index)
 
-operator fun FloatVector.get(index: Int): Float = this.lane(index)
+operator fun IntVector.get(index: Int): Int = lane(index)
 
-operator fun IntVector.get(index: Int): Int = this.lane(index)
+operator fun LongVector.get(index: Int): Long = lane(index)
 
-operator fun LongVector.get(index: Int): Long = this.lane(index)
+operator fun DoubleVector.get(index: Int): Double = lane(index)
 
-operator fun DoubleVector.get(index: Int): Double = this.lane(index)
+operator fun ByteVector.get(index: Int): Byte = lane(index)
 
-operator fun ByteVector.get(index: Int): Byte = this.lane(index)
-
-operator fun ShortVector.get(index: Int): Short = this.lane(index)
+operator fun ShortVector.get(index: Int): Short = lane(index)
